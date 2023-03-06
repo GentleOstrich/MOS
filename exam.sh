b7105986435848740ad77ffc5fafc040781eeb68
Liu Yiheng <273819111@qq.com>
2023-03-06 20:40:31 +0800

    4563

--- exam.sh
+++ exam.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 mkdir mydir
-chmod +rwx mydirt
+chmod a+rwx mydir
+touch myfile
+echo 2023 > myfile
+mv ./moveme ./mydir/
+cp ./copyme ./mydir/
+mv ./mydir/copyme ./mydir/copied
+cat ./readme
+gcc bad.c 2> err.txt
