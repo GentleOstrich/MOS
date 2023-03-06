d8975ae06852430120a9394666dc239d5b05182f
Liu Yiheng <273819111@qq.com>
2023-03-06 20:42:02 +0800

    nouse

--- exam.sh
+++ exam.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 mkdir mydir
-chmod a+rwx mydir
-touch myfile
-echo 2023 > myfile
-mv ./moveme ./mydir/
-cp ./copyme ./mydir/
-mv ./mydir/copyme ./mydir/copied
-cat ./readme
-gcc bad.c 2> err.txt
