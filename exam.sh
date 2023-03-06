452c4432e7fa3a543590d63913981cd56e1a02cd
Liu Yiheng <273819111@qq.com>
2023-03-06 20:08:13 +0800

    111

--- exam.sh
+++ exam.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+mkdir gen
+a=1
+while [$a -le 10]
+do
+	touch ./gen/$a.txt
+	a=$[$a+1]
+done
 mkdir mydir
 chmod +rwx mydir
 touch myfile
@@ -8,12 +15,6 @@ cp copyme ./mydir
 mv ./mydir/copyme ./mydir/copied
 cat ./readme
 gcc bad.c 2> err.txt
-a=1
-while [ $a -ne 10 ]
-do
-	touch ./$a.txt 
-	a=$[$a+1]
-done
 
 
 
